70155c7cc868a519ed3bc944998fafe3b394aa37
RinatTr <rinatreg@gmail.com>
2025-12-21 16:11:23 -0500

    init arrows

--- xy-slider/src/App.jsx
+++ xy-slider/src/App.jsx
@@ -5,7 +5,19 @@ function App() {
   return (
     <>
       <div className="container">
-        <XYController />
+        <div className="controller-wrapper">
+          <div className="label-top">Intense</div>
+          
+          <div className="arrow-container">
+            <div className="arrow arrow-up">▲</div>
+            <div className="arrow arrow-left">◀</div>
+            <XYController />
+            <div className="arrow arrow-right">▶</div>
+            <div className="arrow arrow-down">▼</div>
+          </div>
+          
+          <div className="label-bottom">Expansive</div>
+        </div>
       </div>
     </>
   )
